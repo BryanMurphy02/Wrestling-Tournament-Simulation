1c3263f84bec6814c604d0b034e9ff2b00446f0e
Bryan Murphy <bryanmurphy02@gmail.com>
2022-10-23 23:24:23 -0400

    started setting up bouts

--- src/wrestlingSimulation/bout.java
+++ src/wrestlingSimulation/bout.java
@@ -1,3 +1,23 @@
 package wrestlingSimulation;
 //2 wrestlers and the results of the bout
 //make there be a random chance one or both of the wrestlers get hurt
+// if they have the same strength make it random who wins
+// even if one person is stronger make there be a chance the other person can still win
+
+public class bout{
+
+    //runs a bout between two wrestlers and returns the winnder
+    public wrestlers runBout(wrestlers wrestler1, wrestlers wrestler2){
+
+        //there is a 10% chance that if a wrestler's strength is lower they can win
+
+
+
+        return wrestler1;
+    }
+
+
+
+
+
+}
