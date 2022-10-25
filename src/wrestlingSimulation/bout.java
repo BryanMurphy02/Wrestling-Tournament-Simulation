d5519567801e7e4ef689829e623053b703fc94e5
murphyb18atwit <murphyb18@wit.edu>
2022-10-24 23:25:17 -0400

    added queue to match

--- src/wrestlingSimulation/bout.java
+++ src/wrestlingSimulation/bout.java
@@ -13,9 +13,9 @@ public class bout{
     //runs a bout between two wrestlers and returns the winnder
     public wrestlers runBout(wrestlers wrestler1, wrestlers wrestler2){
 
-        //there is a 10% chance that if a wrestler's strength is lower they can win
+        //there is a 25% chance that if a wrestler's strength is lower they can win
         if(wrestler1.getStrength() > wrestler2.getStrength()){
-            if(chance.getChance(10) == true){
+            if(chance.getChance(25) == true){
                 return wrestler2;
             }
             else{
@@ -23,7 +23,7 @@ public class bout{
             }
         }
         else if(wrestler2.getStrength() > wrestler1.getStrength()){
-            if(chance.getChance(10) == true){
+            if(chance.getChance(25) == true){
                 return wrestler1;
             }
             else{
--- src/wrestlingSimulation/match.java
+++ src/wrestlingSimulation/match.java
@@ -1,3 +1,32 @@
 package wrestlingSimulation;
-// 12 bouts between 2 teams
-// have a queue implemented using a linked list to see who is wrestling next
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class match{
+
+    Queue<wrestlers> line = new LinkedList<>();
+
+    public void makeLine(teams team1, teams team2){
+        // int temp = team1.getWrestlers().size();
+        // for(int i = 0; i < temp; i++){
+        //     ArrayList<wrestlers> team1Wrestlers = team1.getWrestlers();
+        //     line.add(team1Wrestlers.get(i));
+        // }
+
+        // int temp2 = team2.getWrestlers().size();
+        // for(int i = 0; i < temp2; i++){
+        //     ArrayList<wrestlers> team2Wrestlers = team2.getWrestlers();
+        //     line.add(team2Wrestlers.get(i));
+        // }
+        
+    }
+
+    public teams runMatch(teams team1, teams team2){
+        //method for all logic in a math
+        // 12 bouts between 2 teams
+        // have a queue implemented using a linked list to see who is wrestling next
+        return team1;
+    }
+
+}
