4767f24db08a1bb9ae3ec8e54604abc30585a903
murphyb18atwit <bryanmurphy02@gmail.com>
2022-10-30 15:46:18 -0400

    added records to bouts

--- src/wrestlingSimulation/bout.java
+++ src/wrestlingSimulation/bout.java
@@ -8,31 +8,41 @@ public class bout{
 
     //call chance.getChance(int of either 25, 20, or 10)
     chance chance = new chance();
+    scores_records record = new scores_records();
     
 
-    //runs a bout between two wrestlers and returns the winnder
+    //runs a bout between two wrestlers and returns the winner
     public wrestlers runBout(wrestlers wrestler1, wrestlers wrestler2){
 
         //there is a 25% chance that if a wrestler's strength is lower they can win
         if(wrestler1.getStrength() > wrestler2.getStrength()){
             if(chance.getChance(25) == true){
+                record.addWin(wrestler2);
+                record.addLoss(wrestler1);
                 return wrestler2;
             }
             else{
+                record.addWin(wrestler1);
+                record.addLoss(wrestler2);
                 return wrestler1;
             }
         }
         else if(wrestler2.getStrength() > wrestler1.getStrength()){
             if(chance.getChance(25) == true){
+                record.addWin(wrestler1);
+                record.addLoss(wrestler2);
                 return wrestler1;
             }
             else{
+                record.addWin(wrestler2);
+                record.addLoss(wrestler1);
                 return wrestler2;
             }
         }
 
 
-
+        record.addWin(wrestler1);
+        record.addLoss(wrestler2);
         return wrestler1;
     }
 
