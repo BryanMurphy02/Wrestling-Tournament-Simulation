d58dfd7a230ec037672387b0401569801be24a3a
murphyb18atwit <bryanmurphy02@gmail.com>
2022-10-30 15:40:50 -0400

    added record  methods

--- src/wrestlingSimulation/scores_records.java
+++ src/wrestlingSimulation/scores_records.java
@@ -3,9 +3,35 @@ package wrestlingSimulation;
 //wins and losses
 // head to head results in seeding
 
+import java.util.HashMap;
+
+import javax.print.attribute.HashAttributeSet;
+
 public class scores_records{
 
+    HashMap<wrestlers, Integer[]> records = new HashMap<>();
 
+    //method for adding a new wrestler to the HashMap with a record of 0-0
+    public void addWrestler(wrestlers wrestler){
+        Integer[] record = {0, 0};
+        records.put(wrestler, record);
+    }
 
+    //method to add a win the to passed in wrestler's record
+    public void addWin(wrestlers wrestler){
+        if(records.containsKey(wrestler)){
+            Integer[] temp = records.get(wrestler);
+            temp[0]++;
+            records.put(wrestler, temp);
+        }
+    }
 
+    //method to add a loss the to passed in wrestler's record
+    public void addLoss(wrestlers wrestler){
+        if(records.containsKey(wrestler)){
+            Integer[] temp = records.get(wrestler);
+            temp[0]--;
+            records.put(wrestler, temp);
+        }
+    }
 }
